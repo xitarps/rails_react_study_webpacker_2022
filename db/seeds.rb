8fc18c544f5857cea1175530eece3d3a2766783d
Xita Rps <xitarps@gmail.com>
2022-03-03 22:02:55 -0300

    [Feature] Add Event model and seeds
    ```
    rails g model Event title:string datetime:datetime location:string
    ```

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+events = Event.create([
+  {
+    title: "London Retail Expo",
+    datetime: "Mon, 14 Oct 2019",
+    location: "London Excel Centre"
+  },
+  {
+    title: "Enterprise Sales Training Workshop",
+    datetime: "Tue, 15 Oct 2019",
+    location: "Expert Sales Company Headquarters"
+  },
+  {
+    title: "Ruby Hack Night",
+    datetime: "Fri, 18 Oct 2019",
+    location: "Learnetto Headquarters"
+  },
+  {
+    title: "Beginners Salsa dance meetup",
+    datetime: "Sat, 14 Oct 2019",
+    location: "Bar Salsa"
+  }
+])
