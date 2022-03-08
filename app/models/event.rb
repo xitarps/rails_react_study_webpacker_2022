1cc70171c10b6317a8e89d845148c9a7b2d98945
Xita Rps <xitarps@gmail.com>
2022-03-08 00:45:15 -0300

    [Feature]: Add custom validation to event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,12 @@
 class Event < ApplicationRecord
   validates :title, :location, :datetime, presence: true
   validates :title, length: {minimum:3, maximum:25}
+  validate :datetime_cannot_be_in_the_past
+
+  private
+
+  def datetime_cannot_be_in_the_past
+    errors.add(:datetime, "can't be in the past") if (datetime.present? and
+      datetime < DateTime.now)
+  end
 end
